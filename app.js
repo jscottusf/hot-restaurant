5d1c0787689830e9f76fe4c305b535faf36b2023
Carolyn Grady <owenv8@gmail.com>
2020-03-10 20:40:30 -0400

    reservation validation

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 $("#submit").on("click", function (event) {
     event.preventDefault();
-    var newReservation = {
-        customerName = $("#customerName-input").val().trim(),
-        phoneNumber = $("#phoneNumber-input").val().trim(),
-        email = $("#Email-input").val().trim(),
-        uniqueId = $("#uniqueId-input").val().trim(),
+
+    customerName = $("#customerName-input").val().trim();
+    phoneNumber = $("#phoneNumber-input").val().trim();
+    email = $("#Email-input").val().trim();
+    uniqueId = $("#uniqueId-input").val().trim();
+
+    if (validate(customerName, phoneNumber)) {
+        var newReservation = {
+            customerName,
+            phoneNumber,
+            email,
+            uniqueId
+        }
+    }
+    else{
+        alert('Customer name and phone number are required to make a reservation')
     }
 })
 
-function validate(newReservation){
-    console.log(newReservation)
+function validate(customerName, phoneNumber) {
+    
+    if (!customerName || !phoneNumber) {
+        return false;
+    }
 
-    //if (!customerName || !phoneNumber)
+    return true;
 }
